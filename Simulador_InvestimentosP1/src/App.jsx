eb23ce50df8f035f7be1ad455d027475b6b52bac
Anthony Akio <anthony.akio2@gmail.com>
2026-04-16 18:19:05 -0300

    feat: implementa calculo da simulação e limpeza dos campos

--- Simulador_InvestimentosP1/src/App.jsx
+++ Simulador_InvestimentosP1/src/App.jsx
@@ -6,6 +6,50 @@ function App() {
   const [aporteMensal, setAporteMensal] = useState('')
   const [taxaJuros, setTaxaJuros] = useState('')
   const [periodo, setPeriodo] = useState('')
+  const [resultado, setResultado] = useState(null)
+
+  const calcularSimulacao = () => {
+  const P = parseFloat(valorInicial)
+  const A = parseFloat(aporteMensal)
+  const i = parseFloat(taxaJuros) / 100
+  const n = parseInt(periodo)
+
+  if (isNaN(P) || isNaN(A) || isNaN(i) || isNaN(n) || n <= 0) {
+    alert('Preencha os campos corretamente.')
+    return
+  }
+
+  let valorFinal = 0
+
+  if (i === 0) {
+    valorFinal = P + A * n
+  } else {
+    valorFinal = P * Math.pow(1 + i, n) + A * ((Math.pow(1 + i, n) - 1) / i)
+  }
+
+  const totalInvestido = P + A * n
+  const jurosAcumulados = valorFinal - totalInvestido
+  const rentabilidade =
+    totalInvestido > 0 ? (jurosAcumulados / totalInvestido) * 100 : 0
+
+  const novoResultado = {
+    valorFinal,
+    totalInvestido,
+    numeroAportes: n,
+    jurosAcumulados,
+    rentabilidade,
+  }
+  console.log(novoResultado)
+  setResultado(novoResultado)
+  }
+
+  const limparCampos = () => {
+  setValorInicial('')
+  setAporteMensal('')
+  setTaxaJuros('')
+  setPeriodo('')
+  setResultado(null)
+  }
 
   return (
     <div className="container py-4">
@@ -25,8 +69,8 @@ function App() {
             setTaxaJuros={setTaxaJuros}
             periodo={periodo}
             setPeriodo={setPeriodo}
-            aoCalcular={() => {}}
-            aoLimpar={() => {}}
+            aoCalcular={calcularSimulacao}
+            aoLimpar={limparCampos}
           />
         </div>
       </div>
